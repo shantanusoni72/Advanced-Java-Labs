5bf903f225f022638d705751fa057661d9072b18
shaan453 <75296055+shaan453@users.noreply.github.com>
2021-06-04 19:34:39 +0530

    Update cookiesOne.java

--- Cookies/cookiesOne.java
+++ Cookies/cookiesOne.java
@@ -12,7 +12,7 @@ public class CookieOne  extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
              PrintWriter out = response.getWriter();
-              if(request.getParameter("id").equals("ss1234") && request.getParameter("password").equals("secret")) {
+              if(request.getParameter("id").equals("var") && request.getParameter("password").equals("secret")) {
               		response.setContentType("text/html;charset=UTF-8");
               		out.println("<html><head><title>Servlet Cookie</title></head><body><h1>Welcome Cookie</h1><a href='CookieSecond'>Click here</a></body></html>");
              		Cookie cookie = new Cookie("CookieOne",request.getParameter("id"));
